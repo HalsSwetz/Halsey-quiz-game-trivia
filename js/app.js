972f05f59cd28d5f1fa8949e9c9b789bb39544a7
Halsey Svetz <halseycheese@gmail.com>
2025-02-02 13:36:27 -0700

    make pseudocode for building game

---
+++ js/app.js
@@ -0,0 +1,30 @@
+//Steps for building - things needed:
+
+//HTML: questions/ multiple choice answers, submit answer(btn), score total (input)
+
+//CSS: grid? style page and buttons(questions/submit answer/points/win-lose)
+
+//Pseudocode
+//Set up an array of objects containing {question/multiple choices/answer} data
+
+//Set up cached elements - question, choices, submit answer, audio
+
+//Have a scoreboard with a value added for a question answered correctly, and total score
+// -----how does a player lose? negative points for incorrect? need to get 5 correct in a row for winning condition?
+
+//Functions:
+//Click a button and get a random question with  multiple choices
+//Need to be able to choose an answer
+//Need to be able to submit answer
+//Need score to update with correct/incorrect answer
+
+//Need a function for win/lose - show a result. How to win? 
+//If you score > 80% then you win? Or if you get 5 questions in a row you win?
+
+//Need to initialize the game
+//Need a callback to call the audio for 'correct' or 'incorrect' answer
+
+//Event listeners
+//'click' on the different buttons for multiple choices
+//'click' to submit answer
+//
